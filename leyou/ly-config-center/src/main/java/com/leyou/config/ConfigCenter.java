63c407b699ab80c615b8674e606f644438d51cc5
weiqiang <1432114216@qq.com>
2018-11-22 16:13:07 +0800

    整合配置中心

--- leyou/ly-config-center/src/main/java/com/leyou/config/ConfigCenter.java
+++ leyou/ly-config-center/src/main/java/com/leyou/config/ConfigCenter.java
@@ -1,11 +1,24 @@
 package com.leyou.config;
 
+import org.springframework.boot.Banner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
 /**
  * @ClassName ConfigCenter
- * @Description TODO
+ * @Description 配置中心
  * @Author wq
  * @Date 2018/11/22 10:58
  * @Version 1.0.0
  */
+@SpringBootApplication
+@EnableConfigServer
 public class ConfigCenter {
+
+    public static void main(String[] args) {
+        SpringApplication application = new SpringApplication(ConfigCenter.class);
+        application.setBannerMode(Banner.Mode.OFF);
+        application.run(args);
+    }
 }
--- leyou/ly-gateway/src/main/java/com/leyou/ApiGateWay.java
+++ leyou/ly-gateway/src/main/java/com/leyou/ApiGateWay.java
@@ -4,12 +4,16 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @EnableDiscoveryClient
 @EnableZuulProxy
+@RestController
 public class ApiGateWay {
     public static void main(String[] args) {
         SpringApplication.run(ApiGateWay.class);
     }
+
+
 }
